967f8a86d514390536f85d74bc8d4d953802f0c3
StaxGit <lario.free@gmail.com>
2020-05-15 19:59:59 +0200

    Fix typo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const toDoRoutes = require("./routes/todos");
 
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/api/todos", toDoRoutes);
--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const database = require("../models"); //gets all schemas && setups db connection
 
-router.get("/", function(req, res) {
+router.get("/", function(req, res) {;
     database.ToDo.find()
     .then(function(foundToDos) {
         res.json(foundToDos)
