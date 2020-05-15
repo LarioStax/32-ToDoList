5b8ebb1a91bb0af725a9c2538aa335675ad40df4
StaxGit <lario.free@gmail.com>
2020-05-15 19:45:21 +0200

    Add get /api/todos route & setup dotenv

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv").config();
+
+const toDoRoutes = require("./routes/todos");
+
+app.use("/api/todos", toDoRoutes);
 
 app.get("/", function(req, res) {
-    res.send("Hiya!");
-})
+    res.send("Hiya");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function() {
     console.log(`Server listening on port ${port}!`)
-})
+});
---
+++ routes/todos.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const database = require("../models"); //gets all schemas && setups db connection
+
+router.get("/", function(req, res) {
+    database.ToDo.find()
+    .then(function(foundToDos) {
+        res.json(foundToDos)
+    })
+    .catch(function(err) {
+        res.send(err);
+    })
+})
+
+module.exports = router;
