e7d2bf8aa827a3c927daa8bef171099f777946c1
StaxGit <lario.free@gmail.com>
2020-05-15 20:11:26 +0200

    Add post /api/todos route

--- routes/todos.js
+++ routes/todos.js
@@ -2,10 +2,21 @@ const express = require("express");
 const router = express.Router();
 const database = require("../models"); //gets all schemas && setups db connection
 
-router.get("/", function(req, res) {;
+router.get("/", function(req, res) {
     database.ToDo.find()
     .then(function(foundToDos) {
         res.json(foundToDos)
+    })
+    .catch(function(err) {
+        res.send(err);
+    })
+})
+
+router.post("/", function(req, res) {
+    let newToDo = {name: req.body.name};
+    database.ToDo.create(newToDo)
+    .then(function(createdToDo){
+        res.json(createdToDo);
     })
     .catch(function(err) {
         res.send(err);
