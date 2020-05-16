176ecc7c60fd77243e4afd8dd4906d71c2c2250c
StaxGit <lario.free@gmail.com>
2020-05-16 19:57:56 +0200

    Add functionality to create new todos

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,20 +1,41 @@
 $(document).ready(function () {
   $.getJSON("/api/todos")
-    .then(function (data) {
-      console.log(data);
-    })
+    .then(visualiseToDos)
     .catch(function(err) {
       console.log(err);
-    })
+    });
+
+  $("#toDoInput").keypress(function(event) {
+    if(event.which == 13) {
+      createToDo();
+    }
+  })
 })
 
 function visualiseToDos(todos) {
   //add ToDos to page here
   todos.forEach( todo => {
-    let newToDo = $(`<li class="task">${todo.name}</li>`)
-    if (todo.completed) {
-      newToDo.addClass("done");
-    }
-    $(".toDoList").append(newToDo);
+    addToDo(todo);
+  })
+}
+
+function addToDo(todo) {
+  let newToDo = $(`<li class="task">${todo.name}</li>`)
+  if (todo.completed) {
+    newToDo.addClass("done");
+  }
+  $("#toDoList").append(newToDo);
+}
+
+function createToDo() {
+  let userInput = $("#toDoInput").val();
+  //send post request to create new todo
+  $.post("/api/todos", {name: userInput})
+  .then(function(createdToDo) {
+    $("#toDoInput").val("");
+    addToDo(createdToDo);
+  })
+  .catch(function(err) {
+    console.log(err);
   })
 }
